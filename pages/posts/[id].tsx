da893d7dd806cac6dd40fccc99fd4f0262890f1c
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-08-14 16:09:32 +0200

    test

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,6 +2,10 @@ import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { useRouter } from "next/router";
 import { Article } from "@components/Article";
 
+type Params = {
+  id: string;
+};
+
 type Post = {
   userId: number;
   id: number;
@@ -14,6 +18,9 @@ interface Props {
 }
 
 export default function Post({ post }: Props) {
+  if (!post) {
+    return null;
+  }
   const { title, body } = post;
 
   return (
@@ -25,7 +32,7 @@ export default function Post({ post }: Props) {
 }
 
 // This function gets called at build time
-export async function getStaticPaths() {
+export const getStaticPaths = async () => {
   // Call an external API endpoint to get posts
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts: Post[] = await res.json();
@@ -36,12 +43,18 @@ export async function getStaticPaths() {
   }));
 
   return { paths, fallback: false };
-}
+};
 
 // This also gets called at build time
-export async function getStaticProps({ params }: { params: { id: string } }) {
+export const getStaticProps: GetStaticProps = async (context) => {
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
+  const { params } = context;
+  if (!params) {
+    return {
+      props: {},
+    };
+  }
 
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.id}`
@@ -54,4 +67,4 @@ export async function getStaticProps({ params }: { params: { id: string } }) {
       post,
     },
   };
-}
+};
